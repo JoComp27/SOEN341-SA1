c2db6805497f3686a48085b03b1f0d9ce3eed9c3
MattYu <matthew.ming@spaceconcordia.ca>
2018-01-22 18:54:38 -0500

    Create header.php
    Added some php code from Sprint 2 link to do some testings

---
+++ header.php
@@ -0,0 +1,29 @@
+/* 
+Description:
+Header page 
+
+Adapted from open source code: https://code.tutsplus.com/tutorials | Evert Padje
+*/
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl" lang="nl">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="SOEN341 SA1" />
+    <meta name="keywords" content="put, keywords, here" />
+    <title>PHP-MySQL forum</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<h1>My forum</h1>
+    <div id="wrapper">
+    <div id="menu">
+        <a class="item" href="/forum/index.php">Home</a> -
+        <a class="item" href="/forum/create_topic.php">Ask a question</a> -
+        <a class="item" href="/forum/create_cat.php">Question category</a>
+         
+        <div id="userbar">
+        <div id="userbar"> Log out.</div>
+    </div>
+        <div id="content">
