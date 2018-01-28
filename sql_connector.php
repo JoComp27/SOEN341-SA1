2adff013927d474542953aad57c29ee29016361a
MattYu <matthew.ming@spaceconcordia.ca>
2018-01-28 16:35:03 -0500

    Update sql_connector.php
    Added clarifications in comments

--- sql_connector.php
+++ sql_connector.php
@@ -4,7 +4,7 @@
  * 1- Refer to the readme XAMPP sql section for how to setup a mysql server
  * 2- In Xampp mysql, create a new database called "website_db"
  * 3- Run the github .sql query on XAMPP mysql to create tables of "website_db"
- * 4- Include this code to connect to the newly created database using PHP. You can know add, remove, etc. data from PHP to mysql
+ * 4- Include this file to connect to the newly created database using PHP. You can now add, remove, etc. data from PHP to local mysql database on XAMPP
  * User: Ming Tao Yu
  * Date: 1/28/2018
  * Time: 3:29 PM
