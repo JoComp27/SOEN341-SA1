42964085019ed24fbeca4f3623a3fd962b42c27d
MattYu <matthew.ming@spaceconcordia.ca>
2018-02-08 08:01:14 -0500

    Update db_connection.php
    #29: Added chances to the connector to ensure compatibility with the rest of the website

--- App/PHP_HTML/ask_question/db_connection.php
+++ App/PHP_HTML/ask_question/db_connection.php
@@ -1,11 +1,23 @@
 <?php
+/**
+ * How to use:
+ * 1- Refer to the readme XAMPP sql section for how to setup a mysql server
+ * 2- In Xampp mysql, create a new database called "website_db"
+ * 3- Run the github .sql query on XAMPP mysql to create tables for the newly created "website_db"
+ * 4- Use the code below (include) to connect to the newly created database using PHP. 
+ * You may now perform server-side SQL queries on your local db website_db using additional PHP codes 
+ */
+
 function OpenCon() {
-    $conn = new mysqli("localhost","root","1308","okapi_db") or die("Connect failed: %s\n". $conn -> error);
-    return $conn;
+    $user   = 'root';
+    $password   = '';
+    $database   = 'website_db';
+    $db = new mysqli('localhost', $user, $password, $database) or die("Connection failed");
+    return $db;
 }
 
-function CloseCon($conn) {
-    $conn -> close();
+function CloseCon($db) {
+    $db -> close();
 }
 
 ?>
