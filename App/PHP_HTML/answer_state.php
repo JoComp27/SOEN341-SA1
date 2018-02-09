d60a83091f12c2683b8a7ae3795528b61a4ef9d6
Refat Kamal <refat.k17@gmail.com>
2018-02-09 02:16:11 -0500

    #32 removed console.logs #32 added TODO comment

--- App/PHP_HTML/answer_state.php
+++ App/PHP_HTML/answer_state.php
@@ -1,17 +1,14 @@
 <script>
     function checkState(answerNumber) {
-        console.log("answer-state-" + answerNumber);
         var answerStateView = document.getElementById("answer-state-" + answerNumber);
-        console.log(answerStateView);
         var state = parseInt(document.querySelector('input[name="state"]:checked').value);
-
         answerStateView.innerHTML = "<span>Answer has been " + (state === 2 ? "accepted" : "refused") + ".</span>";
     }
 </script>
 
 <div id="answer-state-<?php echo $a ?>" class="answer-state">
     <?php if ($get_answers['answer_state'] == 1) { ?>
-        <form class="answer-state-form" name="answerState" action="">
+        <form class="answer-state-form" name="answerState" action=""> <!--TODO: Complete Backend-->
             <label><input type="radio" name="state" value="2" onchange="checkState(<?php echo $a ?>)">
                 Accept Answer</label>&nbsp;
             <label><input type="radio" name="state" value="0" onchange="checkState(<?php echo $a ?>)">
