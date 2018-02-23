2e1880e8aa2d9d3e8ed7f947c9b422c7edfe626f
MattYu <matthew.ming@spaceconcordia.ca>
2018-02-23 14:10:07 -0500

    #51 Removed todo comment

--- App/PHP_HTML/answer_state.php
+++ App/PHP_HTML/answer_state.php
@@ -8,7 +8,7 @@
 
 <div id="answer-state-<?php echo $a ?>" class="answer-state">
     <?php if ($get_answers['answer_state'] == 1) { ?>
-        <form class="answer-state-form" name="answerState" action="answer_state_2.php?id=<?php echo $get_answers['answers_id']?>&question_id==<?php echo $get_answers['reply_questions'] ?>" method="post""> <!--TODO: Complete Backend-->
+        <form class="answer-state-form" name="answerState" action="answer_state_2.php?id=<?php echo $get_answers['answers_id']?>&question_id==<?php echo $get_answers['reply_questions'] ?>" method="post"">
             <label><input type="submit" name="state" value="2" onchange="checkState(<?php echo $a ?>); this.form.submit()">
                 Accept Answer</label>&nbsp;
             <label><input type="submit" name="state" value="0" onchange="checkState(<?php echo $a ?>); this.form.submit()">
