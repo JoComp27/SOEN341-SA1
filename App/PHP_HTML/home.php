3422c8aff667084ad0b3ba6eaa2a9efeee09fb43
Jonathan Cournoyer <jonathan.cournoyer27@gmail.com>
2018-02-25 00:33:24 -0500

    Started to add the tags input box for adding tags to ind. questions
    Started to make the input box for inputting tags to individual questions in the add question section

--- App/PHP_HTML/home.php
+++ App/PHP_HTML/home.php
@@ -9,13 +9,17 @@
             <button id="ask" type="button">Ask a Question!</button>
             <br>
             <p id="question_field" name="question_field" class="hidden">
-                <strong id="title_title" class="pretty">Title</strong>
+                <strong id="title_title" class="pretty">Title : </strong>
                 <br>
                 <input type="text" name="title">
                 <br>
-                <strong id="details_title">Details</strong>
+                <strong id="details_title">Details : </strong>
                 <br>
                 <textarea rows="4" cols="50" name="details"></textarea>
+                <br>
+                <strong id="tags_title">Associated Tags : </strong>
+                <br>
+                <input type="text" name="tags" data-role="tagsinput" placeholder="Add tags">
                 <br>
                 <input id="submit" type="submit">
             </p>
