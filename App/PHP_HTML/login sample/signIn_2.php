edc913e3d46e4ea89beaf4c8e92bf2941a18c5c9
Jessica Allaire <jessica.allaire.96@hotmail.com>
2018-03-06 17:52:40 -0600

    #79 Changed Header Logo
    Changed the logo to the one without the white background. AKA Logo to Logo3

--- App/PHP_HTML/login sample/signIn_2.php
+++ App/PHP_HTML/login sample/signIn_2.php
@@ -21,7 +21,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="../home.php"><img id="img" src="logo.png"></a>
+            <a class="navbar-brand" href="../home.php"><img id="img" src="logo3.png"></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
