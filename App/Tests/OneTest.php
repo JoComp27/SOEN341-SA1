7d3f19d270b9ea5894f13f69aa200a32223d592e
Jonathan Cournoyer <jonathan.cournoyer27@gmail.com>
2018-03-07 16:02:35 -0500

    #110 Added Test configs for the testing environment of the project
    - Added mysql in the travisCI config
    - Added a simple test test
    - updated Composer
    - Set up the phpunit correctly

---
+++ App/Tests/OneTest.php
@@ -0,0 +1,15 @@
+<?php
+
+//use PHPUnit\Framework\TestCase;
+
+class OneTest extends PHPUnit_Framework_TestCase{
+
+    public function testTest1(){
+
+        $this->assertTrue(true);
+
+    }
+
+}
+
+?>
