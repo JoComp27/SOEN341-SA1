5613691cc2ad98ea5d3ed23077ef3c0bf5bc8989
MattYu <matthew.ming@spaceconcordia.ca>
2018-03-07 16:37:57 -0500

    #110: Added travis github extension

--- App/Tests/SessionTest.php
+++ App/Tests/SessionTest.php
@@ -2,7 +2,7 @@
 session_start();
 //use PHPUnit\Framework\TestCase;
 
-class SessionTest extends PHPUnit_Framework_TestCase{
+class SessionTest extends PHPUnit\Framework\TestCase{
 
     public function testTest1(){
     	
