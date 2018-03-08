192a3d0d53fa36c22d95dfdb22cd926433fcf1d4
Refat Kamal <refat.k17@gmail.com>
2018-03-08 00:24:24 -0500

    #124 refactored delete question to be more readable

--- App/PHP_HTML/deleteQuestion/delete_question_action.php
+++ App/PHP_HTML/deleteQuestion/delete_question_action.php
@@ -3,10 +3,10 @@
 // PURPOSE: runs query that deletes a question
 
 <?php
-    $DELETE_QUESTION = 1;
+    $DELETED = 1;
     $questionId = $_POST['questionId'];
 
-    $query = "UPDATE questions SET question_deleted = $DELETE_QUESTION WHERE question_id = '$questionId'";
+    $query = "UPDATE questions SET question_deleted = $DELETED WHERE question_id = '$questionId'";
     mysqli_query($db, $query) or die(mysqli_error($db));
     $redirect = 'Location: ../home.php'; // go back to home page once answer deleted
     header($redirect);
