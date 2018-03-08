9a5928a30131d33b5927fca65e43f62f1cf6a2b3
Refat Kamal <refat.k17@gmail.com>
2018-03-08 00:44:19 -0500

    #124 fixed issue where deleting question did not correctly redirect

--- App/PHP_HTML/deleteAnswer/delete_answer_action.php
+++ App/PHP_HTML/deleteAnswer/delete_answer_action.php
@@ -5,9 +5,10 @@
 <?php
     $DELETED = 1;
     $answerId = $_POST['answerId'];
+    $qus_id = $_POST['questionId'];
 
     $query = "UPDATE answers SET answer_deleted = $DELETED WHERE answers_id = '$answerId'";
     mysqli_query($db, $query) or die(mysqli_error($db));
-    $redirect = 'Location: ../answer.php?id=' . $ques_id; // go back to question page once answer deleted
+    $redirect = 'Location: ../answer.php?id=' . $qus_id; // go back to question page once answer deleted
     header($redirect);
 ?>
