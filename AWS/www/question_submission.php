62cef2cfb4863d6c5c9aa4efe71ef7e6732d78d6
MattYu <matthew.ming@spaceconcordia.ca>
2018-03-21 23:29:13 -0400

    #98 : Stable AWS v2 withou phpunit

---
+++ AWS/www/question_submission.php
@@ -0,0 +1,62 @@
+<?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="ask_question.js"></script>
+    <?php include "header.php" ?>
+</head>
+<body>
+<?php
+
+if (isset($_SESSION['auth'])) {
+    $title = $_POST["title"];
+    $details = $_POST["details"];
+    $t = $_SESSION['user_id'];
+    $tags = $_POST["tags"];
+    $question_by_user = $_SESSION['user_name'];
+    $sql = "INSERT INTO questions (question_by, question_title, question_description, question_date, question_by_user) VALUES ($t, \"$title\", \"$details\", NOW(), '$question_by_user')";
+    $db->query($sql);
+
+    $sql = "select question_id from questions where question_by = \"$t\" and question_title = \"$title\"";
+    $result = $db->query($sql);
+    $row = $result->fetch_assoc();
+    $id = $row['question_id'];
+
+    $intQId = intval($id);
+
+    $tagArray = explode(",", $tags);
+
+    foreach ($tagArray as $tag) {
+        // Iterates through every tag, inserts it in the tags table and
+        // makes the Q to T association in the question_tags table
+        $sql = " INSERT INTO tags (tag_name) VALUES (\"$tag\") ";
+        $db->query($sql);
+
+        $sql = " SELECT tag_id FROM tags WHERE tag_name = \"$tag\"";
+        $result = $db->query($sql);
+        $Tid = $result->fetch_row();
+        $Tid_int = intval($Tid[0]);
+
+        $sql = "INSERT INTO question_tags (question_id, tag_id) VAlUES($intQId, $Tid_int)";
+        $db->query($sql);
+    }
+
+    $url = "answer.php?id=$intQId";
+    ?>
+    <script type="text/javascript">
+       window.location.href = "<?php echo $url?>"
+    </script>
+<?php
+    exit;
+} else {
+    echo "<div class='alert alert-danger'><strong>Error!</strong> Please Log in/Sign up to post a question.</div>";
+}
+
+?>
+</body>
+</html>
---
+++ AWS/www/search.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include "header.php" ?>
+</head>
+</html>
+<body>
+<div class="container">
+    <h2>The Search Results</h2>
+
+    <ul class="nav nav-tabs">
+        <li class="active"><a data-toggle="tab" href="#home">Questions</a></li>
+    </ul>
+
+    <div id="home" class="tab-pane fade in active">
+        <div class="tab-content">
+            <table class="table">
+
+
+                <?php
+                $query = trim($_GET['query']);
+
+                //echo "query is ".$query;
+                // gets value sent over search form
+
+                $min_length = 1;
+                // you can set minimum length of the query if you want
+
+                if (strlen($query) >= $min_length) { // if query length is more or equal minimum length then
+
+                    $search_query = "select questions.question_id,questions.question_view_count,questions.question_title, questions.question_date, 
+							questions.question_upvote, ifnull(count(answers.answers_id),0) as total
+							from questions left join answers on (answers.reply_questions = questions.question_id)
+							where (questions.question_description like '%$query%' OR questions.question_title like '%$query%') 
+							group by questions.question_id,questions.question_view_count,questions.question_title, questions.question_date, questions.question_upvote;";
+                    $question_data = mysqli_query($db, $search_query) or die(mysqli_error($db));
+
+                    if (mysqli_num_rows($question_data) == 0) {
+                        echo "No questions found which contain the text: '$query'";
+                    } else {
+                        echo nl2br("\r\nThe following questions have been found that match your text :'" . $query . "'\r\n\n");
+
+                        while ($row = mysqli_fetch_array($question_data)) {
+                            echo '<tr>
+              <td>
+                <div class = "col-md-1">
+                  <p class = "top-questions-stats">' . $row["question_upvote"] . '</p>
+                </div>
+                <div class = "col-md-1">
+                  <p class = "top-questions-stats">' . $row["total"] . '</p>
+                </div>
+                <div class = "col-md-1">
+                  <p class = "top-questions-stats">' . $row["question_view_count"] . '</p>
+                </div>
+                <div class = "col-md-9">
+                  <a href = "answer.php?id=' . $row["question_id"] . ' " target = "blank"><h4>' . $row["question_title"] . '</h4></a>
+                </div>
+              </td>
+            </tr>
+            <tr class="warning borderless">
+              <td>
+                <div class = "col-md-1">
+                  <p class = "top-questions-stats">votes</p>
+                </div>
+                <div class = "col-md-1">
+                  <p class = "top-questions-stats">answer</p>
+                </div>
+                <div class = "col-md-1">
+                  <p class = "top-questions-stats">views</p>
+                </div>
+                <div class = "col-md-2">
+                  <p>tags</p>
+                </div>
+                <div class = "col-md-7">
+                  <p class = "asked"> asked on ' . $row["question_date"] . '</p>
+                </div>
+              </td>
+            </tr> <!-- end row 2 -->';
+                        }
+                    }
+                } else { // if an empty string was searched
+                    echo "Please enter at least one character to search";
+                }
+                ?>
+</body>
+</html>
