24eebcc106394a4ffc5534e6be8ace3e29c6eee2
MattYu <matthew.ming@spaceconcordia.ca>
2018-03-22 21:55:18 -0400

    #146 : Add phpunit test v1.2.7

--- App/Tests/LogoutTest.php
+++ App/Tests/LogoutTest.php
@@ -6,7 +6,6 @@ class LogoutTest extends PHPUnit\Framework\TestCase{
     public function testTest1(){
     	if (!isset($_SESSION)){
 			$this->assertFalse(False);
-			session_start();
 			$_SESSION = array('auth' => 'True', 'user_name' => 'Test', 'user_id' => '1');
 			
 		}
