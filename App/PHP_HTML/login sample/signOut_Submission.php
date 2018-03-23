4f6a109484cb891794175c877e8b94a2429213ed
MattYu <matthew.ming@spaceconcordia.ca>
2018-03-22 22:05:15 -0400

    #146 : Add phpunit test v1.2.9

--- App/PHP_HTML/login sample/signOut_Submission.php
+++ App/PHP_HTML/login sample/signOut_Submission.php
@@ -12,13 +12,13 @@ $_SESSION = array();
 
 // If it's desired to kill the session, also delete the session cookie.
 // Note: This will destroy the session, and not just the session data!
-if (ini_get("session.use_cookies")) {
+/*if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
         $params["path"], $params["domain"]//,
         //$params["secure"], $params["httponly"]
     );
-}
+}*/
 
 // Finally, destroy the session.
 session_destroy();
