8d404b2f990f8a57c1e4851173e637b646b62206
MattYu <mingtyut@gmail.com>
2018-04-12 00:00:04 -0400

    #188 fix mapping syntax error 2

--- App/Tests/Sign_upTest.php
+++ App/Tests/Sign_upTest.php
@@ -90,9 +90,10 @@ class Sign_upTest extends PHPUnit\Framework\TestCase{
 		$db->query($query);
 
 		//****************************************************************************************
+		// Section pertain to test branch: SUCCESSFUL sign up
 		/* Creating mock POST data */
 		// Below is the mock user input for branch success case:
-		$_POST = array("submitform" = true,
+		$_POST = array("submitform" => true,
 				"user_name" => "test_user_1",
 				"user_email" => "test1@gmail.com",
 				"answer1" => "secret1",
@@ -103,8 +104,7 @@ class Sign_upTest extends PHPUnit\Framework\TestCase{
 				"day" => 1,
 				"gender" => "M",
 				"cpassword" => "password1",
-				"user_pass" => "password1",
-				);
+				"user_pass" => "password1");
 
 		//***************************************************************************************
 		/*Collect baseline stats pre test*/
@@ -191,6 +191,110 @@ class Sign_upTest extends PHPUnit\Framework\TestCase{
     	$this->assertTrue($comparator_count2 == ($baseline_count2 + 1));
     	$this->assertTrue($comparator_count2 == ($baseline_count2 + 1));
 
+
+
+    	//****************************************************************************************
+		// Section pertain to test branch: Fail sign up due to user_name unavailable
+		/* Creating mock POST data */
+		// Below is the mock user input for branch success case:
+		$_POST = array("submitform" = true,
+				"user_name" => "test_user_1",
+				"user_email" => "test2@gmail.com",
+				"answer1" => "secret1",
+				"answer2" => "secret2",
+				"answer3" => "secret2",
+				"year" => 2005,
+				"month" => 11,
+				"day" => 1,
+				"gender" => "M",
+				"cpassword" => "password1",
+				"user_pass" => "password1",
+				);
+
+		//***************************************************************************************
+		/*Collect baseline stats pre test*/
+    	$baseline_count1=mysqli_num_rows($db->query("select * from users"));
+    	$baseline_count2=mysqli_num_rows($db->query("select * from notification"));
+    	$baseline_count3=mysqli_num_rows($db->query("select * from notification"));
+
+
+    	/****************************************************************************************
+		TEST BEGIN: Branch fail creation due to username unavailable
+
+		Running source code replicat from sign_up.php line 14 to 74 below: 
+
+    	*****************************************************************************************/
+
+
+		if (isset($_POST['submitform']) && $_POST['user_pass'] == $_POST['cpassword']) {
+
+		    $user_name = mysqli_real_escape_string($db, $_POST['user_name']);
+		    $user_email = mysqli_real_escape_string($db, $_POST['user_email']);
+
+
+		    $user_check_query = "SELECT * FROM users WHERE user_name ='$user_name' OR user_email='$user_email' LIMIT 1";
+		    $result = mysqli_query($db, $user_check_query);
+		    $user = mysqli_fetch_assoc($result);
+
+		    if ($user) { // if user exists
+		        if ($user['user_name'] === $user_name) {
+		            ?>
+		            <script type="text/javascript">alert("username already exist");</script>
+		            <?php
+		        }
+
+		        if ($user['user_email'] === $user_email) {
+		            ?>
+		            <script type="text/javascript">alert("email already exist");</script>
+		            <?php
+		        }
+		    }
+
+		    $answer1 = $_POST['answer1'];
+		    $answer2 = $_POST['answer2'];
+		    $answer3 = $_POST['answer3'];
+		    $user_pass = md5($_POST['user_pass']);
+		    $dateOfBirth = $_POST['year'] . "-" . $_POST['month'] . "-" . $_POST['day'];
+		    $gender = $_POST['gender'];
+
+
+		    $query = "INSERT INTO `users` (user_name, user_pass, user_email, user_birthDate, user_gender, user_date, user_answer1, user_answer2, user_answer3) VALUES ('$user_name', '$user_pass', '$user_email', '$dateOfBirth', '$gender', now(), '$answer1', '$answer2', '$answer3')";
+		    $result = mysqli_query($db, $query);
+
+		    //once sign up is successful, automatically log the user in and generate a welcome message with session
+		    $_SESSION['auth'] = "True";
+		    $_SESSION['user_name'] = $user_name;
+		    $user_id_check_query = "SELECT user_id FROM users WHERE user_name = '$user_name' AND user_email='$user_email' LIMIT 1";
+		    $result = mysqli_query($db, $user_id_check_query);
+		    $user_id = mysqli_fetch_assoc($result);
+		    $_SESSION['user_id'] = $user_id['user_id'];
+
+
+		    //add a welcome message to user inbox
+
+		    $query = "insert into notification (notification_title, notification_date, notification_content) values('Welcome to Okapi!',NOW(), 'Hi $user_name, <br/> <br/> Welcome to Okapi.com, a platform that allows user to exchange questions and share knowledges! <br/> <br/> On behalf of the Okapi team, we wish you a pleasant journey. <br/><br/> Sincerely, <br/> Team Okapi')";
+		    $notice_result = mysqli_query($db, $query);
+		    $latest_local_notification_id = mysqli_fetch_assoc(mysqli_query($db, "SELECT LAST_INSERT_ID() as 'result'"))['result'];
+		    $user_id = $user_id['user_id'];
+		    $sql = "insert into notification_user (notification_id, user_id) values ('$latest_local_notification_id', '$user_id')";
+		    mysqli_query($db, $sql);
+		    // TO CHANGE PATH ON AWS
+		    header('Location: ../home.php');
+		} else if (isset($_POST['submitform']) && $_POST['user_pass'] != $_POST['cpassword']) {
+		    ?>
+		    <script type="text/javascript">alert("two passwords do not match. Try again!");</script>
+		    <?php
+		}
+
+       	
+       	//******************************************************************************************
+       	// Asserting results of successful tests. 
+       	// Expected output: all data count should remain the same as no new user should be created. 
+       	// Expected results: assertTrue x 3
+
+    	$this->assertTrue($comparator_count1 == ($baseline_count1));
+    	$this->assertTrue($comparator_count2 == ($baseline_count2));
+    	$this->assertTrue($comparator_count2 == ($baseline_count2));
 }
 }
 
