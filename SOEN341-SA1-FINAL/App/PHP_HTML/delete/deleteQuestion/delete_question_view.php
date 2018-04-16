e0c35b85c88a7f0c2f3bcbf8294ffefd222ee861
Jonathan Cournoyer <jonathan.cournoyer27@gmail.com>
2018-04-16 11:06:31 -0400

    #192 - Removed unauthorised master push
    Will remake changes using a pull request

--- SOEN341-SA1-FINAL/App/PHP_HTML/delete/deleteQuestion/delete_question_view.php
+++
@@ -1,7 +0,0 @@
-<!-- PURPOSE: Shows the delete question button which allows user to delete question -->
-
-<form name="deleteQuestion" action="../delete/deleteQuestion/delete_question_action.php" method="post">
-    <!-- As long as $qus_id is set, this action will work  -->
-    <input type="hidden" name="questionId" value="<?php echo $qus_id ?>">
-    <input type="submit" value="Delete">
-</form>
